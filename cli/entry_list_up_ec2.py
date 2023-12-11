0177135ffa8d1389d453de7910152feeb55e1c3c
Khoi Thinh <thinhduckhoi@gmail.com>
2023-12-11 17:08:52 +0900

    Update entry_list_up_ec2.py

--- cli/entry_list_up_ec2.py
+++ cli/entry_list_up_ec2.py
@@ -1,4 +1,18 @@
-#Get input from option -p and call the function inside list_up_ec2 script (same directory) to output the result
+"""
+Get input from option -p and call the function inside list_up_ec2 script (same directory) to output the result
+
+Usage:
+./entry_list_up_ec2.py -p windows
+{'SuperWin': 'Windows'}
+
+./entry_list_up_ec2.py -p Linux
+{'aws-cloud9-terraform-1-3-e057eb8730054752bf43bc99d1ce88ac': 'Linux/UNIX', 'aws-cloud9-ide3-55bd06969e704acf9001b9cc25b5ebb7': 'Linux/UNIX', 'master-node': 'Linux/UNIX', 'node-01': 'Linux/UNIX'}
+
+./entry_list_up_ec2.py -p
+usage: entry_list_up_ec2.py [-h] [-p PLATFORM]
+entry_list_up_ec2.py: error: argument -p/--platform: expected one argument
+
+"""
 #!/usr/bin/python3
 
 import argparse
